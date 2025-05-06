d98f111b5d8610498e2fbf56ce639fff22e31591
pranav9gupta <pranav09261630@gmail.com>
2025-05-06 10:33:26 +0530

    commit with inserting and getting from DB using JDBC

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+import java.sql.*;
+
+public class Main {
+
+    private static final String url = "jdbc:mysql://localhost:3306/mydb";
+    private static final String username = "root";
+    private static final String password = "@TCO7b2dx";
+
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
         }
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            String query2 = "INSERT INTO students(name, age, marks) VALUES ('Ashu', 32, 90.5);";
+            int result = statement.executeUpdate(query2);
+            if(result > 0){
+                System.out.println("Record inserted successfully");
+            }else {
+                System.out.println("Record not inserted");
+            }
+            String query = "SELECT * FROM students";
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                double marks = rs.getDouble("marks");
+                System.out.println(id + ", " + name + ", " + age + ", " + marks);
+
+            }
+
+
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
